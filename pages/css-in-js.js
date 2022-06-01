2931760f472b327b3e169df99e19c6156412d66e
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-01 17:31:41 +0400

    added style components

---
+++ pages/css-in-js.js
@@ -0,0 +1,7 @@
+import { Title } from "../styles/Title.styles";
+
+const CSSJS = () => {
+  return <Title color="black">Styled Components</Title>;
+};
+
+export default CSSJS;
---
+++ styles/Title.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 50px;
+  color: darkgreen;
+  background-color: ${(props) => props.color};
+`;
+
+export { Title };
