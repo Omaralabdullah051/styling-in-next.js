08466f0b55bf72407ff69145c08b01077fb2f638
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-01 16:48:48 +0400

    added component level styles

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,12 @@
+import styles from "../styles/About.module.css";
+
 const About = () => {
-  return <h2>About Page</h2>;
+  return (
+    <>
+      <h2>About Page</h2>
+      <h5 className={styles.highlight}>About</h5>
+    </>
+  );
 };
 
 export default About;
--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,12 @@
+import styles from "../styles/Contact.module.css";
+
 const Contact = () => {
-  return <h2>Contact Page</h2>;
+  return (
+    <>
+      <h2>Contact Page</h2>
+      <h5 className={styles.highlight}>Contact</h5>
+    </>
+  );
 };
 
 export default Contact;
